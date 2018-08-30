79a1bac0a6c87159aa920a095f59868e833dd4c0
Ubuntu <ubuntu@ip-172-31-37-68.eu-central-1.compute.internal>
2018-08-30 11:13:31 +0000

    fix virtualenv

--- collector-env/bin/activate.fish
+++ collector-env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ubuntu/twitter_stream/twitter_env"
+set -gx VIRTUAL_ENV "/home/ubuntu/tweestat/tweestat-collector/collector-env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
